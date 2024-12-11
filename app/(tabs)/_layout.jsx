757cfc1c8a61f61eed852682043b3941bf660a87
Santiago Castellanos <sjcastellanos0204@gmail.com>
2024-12-11 12:04:55 -0700

    Added bottom nav bar along with pics/icons for assets folder

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,88 @@
+import { Text, View, Image } from "react-native";
+import { Tabs, Redirect } from "expo-router";
+import { icons } from "../../constants";
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`$focused ? 'font-psemi-bold' : 'font-pregular'} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001", //change this color to green
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            borderTopWidth: 1,
+            height: 84,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="journal"
+          options={{
+            title: "Journal",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                name="Journal"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                name="Profile"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
